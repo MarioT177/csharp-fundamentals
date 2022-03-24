e288e3b70eff1e1b781cc2aee3471fe176551e0a
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-03-24 18:42:06 -0500

    CAC-103 C# Overview

---
+++ MyFirstApplication/MyFirstApplication/House.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MyFirstApplication;
+
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("My door is open.");
+    }
+
+}
+
