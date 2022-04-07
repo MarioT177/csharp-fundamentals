0ffd8ce31c06b078899fb64553b2cc179266288f
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-04-07 14:58:55 -0500

    CAC-103 Exercise4

---
+++ MyFirstApplication/MyFirstApplication/Exercises/Exercise4.cs
@@ -0,0 +1,77 @@
+﻿
+
+namespace MyFirstApplication;
+
+internal class Exercise4
+
+/*
+ problem 1 - Write a method that takes 2 string arguments and void 
+return type. Using and if-else statement, compare the two strings to each other
+Using a Console WriteLine statement, print out the results of whether 
+the strings are equal. Provide a comment inside the method that says what strings
+you used during testing
+ */
+
+{
+     public void IfElseProblem(string able, string beta)
+    {
+        if(able == beta)
+        {
+            Console.WriteLine($"{able} is equal to {beta} ");
+        }
+        else
+        {
+            Console.WriteLine($"{able} is greater than {beta}");
+        }
+        
+   }
+
+/* problem 2 - Write a method that will take 1 char arguement and 
+ a string return type.Using a switch and the table below, return the 
+description. If the value does not match the grade, return the defualt 
+message, "Not a valid grade"
+
+ */
+
+    public void SwitchProblem(char A)
+    {
+        switch (A)
+        {
+            case 1:
+                Console.WriteLine($"{V} is a not vaid grade");
+                break;
+            case 2:
+                Console.WriteLine($"{E} is not a valid grade");
+                break;
+            case 3:
+                Console.WriteLine($"{G} is not a valid grade");
+                break;
+            case 4:
+                Console.WriteLine($"{F} is not a valid grade");
+                break;
+            default:
+                Console.WriteLine($"{A} is a valid grade");
+
+
+        }
+
+
+
+    }
+
+    /*
+           problem 3 - Write a method that takes no arguements and a void
+        return type. Using an iteration statement, display all the numbers divisble
+        by 3 from 0 to 30.Utilize a modulus to obtain your results. 
+         */
+    // could not complete
+    public void IterationStatement()
+    {
+
+    }
+
+
+
+
+
+}
