1928e90a013f377a9b07f67ed13aa57382b788db
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-04-16 09:32:58 -0500

    CAC-103 Exercise 10

---
+++ MyFirstApplication/MyFirstApplication/Exercises/Boat.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace MyFirstApplication;
+
+// Exercise 10 Encapsulation 
+// This lab goes over encapsulation 
+
+
+internal class Boat
+
+/*
+ Problem 2 - Create a class based on your Boat object. You will follow the same
+instructions as exercise 1 except for the following. Include your attributes
+as Expression Body Definition Properties.
+ 
+ */
+
+
+
+{
+    private string _anchorSize;
+    private string _boatColor;
+    private string _boatType;
+    private int boatLength;
+
+    public string AnchorSize
+    {
+        get => _anchorSize;
+        set => _anchorSize = value;
+    }
+
+    public string BoatColor
+    {
+        get => _boatColor;
+        set => _boatColor = value;
+    }
+
+    public string BoatType
+    {
+        get => _boatType;
+        set => _boatType = value;
+    }
+
+    public int BoatLength
+    {
+        get => boatLength;
+        set => boatLength = value;
+
+    }
+
+    public Boat() 
+        :this("white", "medium")
+    { }
+
+
+    public Boat(string paintColor, string anchorSize)
+        : this(paintColor, anchorSize, "White", 20)
+    { }
+
+
+    public Boat(string anchorSize, string boatColor, string boatType, int boatLength)
+    {
+        AnchorSize = anchorSize;
+        BoatColor = boatColor;
+        BoatType = boatType;
+        BoatLength = boatLength;    
+    }
+public void BoatMake()
+    {
+        Console.WriteLine($"The {BoatType} is a tug boat");
+    }
+
+}
+
+
+
+
+
+
---
+++ MyFirstApplication/MyFirstApplication/Exercises/Horse.cs
@@ -0,0 +1,54 @@
+﻿namespace MyFirstApplication;
+
+
+// Exercise 10 Encapsulation 
+// This lab goes over encapsulation
+
+
+internal class Horse
+/*Problem 1 - Create a class based on your Horse object. Include your attributes as auto properties.
+  Create 3 constructors. The first constructor should have parameters equal to your properties.
+  Assign each parameter to your properties. The second constructor should have 
+  2 parameters. Using the :this() keyword, you will pass your information to your first constructor,you 
+  will create the defualt constructor. Using the :this() keyword, you will pass
+  2 defualt values in that will pass to the 2nd constructor. Also include in the class
+  your method for Horse.
+ 
+ */
+
+
+
+
+{
+    public string HorseColor { get; set; }
+    public string TailLength { get; set; }
+    public string HorseType { get; set; }
+    public int HorseWieght { get; set; }
+
+
+    public Horse()
+        :this("white", "long")
+        { }
+
+
+    public Horse(string horseColor, string tailLength)
+        :this(horseColor, tailLength, "Mustang", 20)
+        { }
+
+
+    public Horse(string horseColor, string tailLength, string horseType, int horseWeight)
+    {
+        HorseColor = horseColor;
+        TailLength = tailLength;
+        HorseType = horseType;
+        HorseWieght = horseWeight;
+
+    }
+
+    public void HorseSpecies()
+    {
+        Console.WriteLine($"My horse is a {HorseType}");
+    }
+
+
+}
