acd8dc102ca858bb43acb1f93e3fad99ef4b5561
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-04-18 10:31:56 -0500

    CAC-103-Lesson 11

---
+++ MyFirstApplication/MyFirstApplication/Exercises/BoatChildClass.cs
@@ -0,0 +1,33 @@
+﻿namespace MyFirstApplication;
+
+public class ChildBoat
+{
+    private string _rowBoat;
+
+
+    public string RowBoat
+    {
+        get => _rowBoat;
+        set => _rowBoat = value;
+    }
+
+
+    public ChildBoat()
+       : this("Small", " Fast")
+    { }
+
+    public ChildBoat(string anchorSize, string boatType, string rowBoat)
+        : this(anchorSize, boatType, "Tug boat", "Small")
+    { }
+
+    public ChildBoat(string anchorSize, string boatColor, string boatType, string rowBoat )
+    
+        :base (anchorSize, boatColor, boatType, rowBoat);
+    { }
+
+public void ChildBoatType()
+{
+    Console.WriteLine($"A  is a {rowBoat} version of a Boat");
+}
+
+}
---
+++ MyFirstApplication/MyFirstApplication/Exercises/HorseChildClass.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace MyFirstApplication;
+/*
+ Create a child class based on your Horse object. Include one child attribute as an 
+auto property. Create 3 constructors. The first constructor should have 4 parameters with 
+3 of them from Horse and your child property. Using the keyword:Base(), you will 
+put the four horse parameters in the parenthesis. In the body of the constructor you 
+will assign your child parameters to your property.
+
+The second constructor should have 2 parameters, 2 based on the horse and 1 from
+your child. Using the :this() keyword, you will pass 2 of the parent attributes 
+and your child attribute information to the first constructor. Include defualt values
+to represent the 2 remaining horse parameters.
+
+For the last constructor, you will create the defualt constructor. Using the 
+:this() keyword, you will pass 2 defualt values in that will pass the 2nd constructor.
+Also include in your class, your method for your child class
+ 
+ 
+ */
+public class Pony
+{
+    protected string PonySize { get; set; }
+
+    public Pony()
+        : this("Small", "Brown")
+    { }
+
+
+
+    public Pony(string horseColor, string tailLength, string PonySize)
+        : this(horseColor, tailLength, "Mustang", 20, "Small")
+    { }
+
+
+
+
+    public Pony(string horseColor, string tailLength, string horseType, string ponySize)
+
+         :base(horseColor, tailLength, horseType, ponySize)
+    { 
+          PonySize = ponySize;
+    }
+
+    public void PonySpecies()
+    {
+        Console.WriteLine($"A pony is a {PonySize} version of a Horse");
+    }
+
+
+
+
+
+}// end class
