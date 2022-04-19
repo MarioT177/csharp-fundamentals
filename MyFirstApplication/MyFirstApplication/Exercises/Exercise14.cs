1b859389996d3fabcd00faeb0c94d56f10ee2168
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-04-19 12:13:49 -0500

    CAC-103-Exercise14

---
+++ MyFirstApplication/MyFirstApplication/Exercises/Exercise14.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace MyFirstApplication;
+
+internal class Exercise14
+{
+    public void ExerciseSingleArray()
+    {
+
+        string[] singleArray = new string[3];
+        singleArray[0] = "mint choclate";
+        singleArray[1] = "rocky road";
+        singleArray[2] = "sherbert";
+
+        foreach (string icecream in singleArray)
+        {
+            Console.WriteLine($"my favorite {icecream} flavor is mint choclate");
+
+        }
+
+    }
+    public void FootballArray()
+    {
+        int[,] footballScore = new int[0, 0];
+        footballScore[6, 3] = 1;
+        footballScore[13, 0] = 2;
+        footballScore[3, 7] = 3;
+        footballScore[0, 7] = 4;
+        footballScore[17, 22] = 5;
+        foreach (int finalscore in footballScore)
+        {
+            Console.Write($"{finalscore}, ");
+        }
+
+
+
+    }
+
+
+}
