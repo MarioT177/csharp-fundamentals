6fd4d353b4fe2b9ed7f2d4106018b9d0f7d0bab3
Mario Trujillo <mariotrujillo1717@gmail.com>
2022-04-21 11:58:08 -0500

    CAC-103 Exercise 17

--- MyFirstTestProject/Lesson10test.cs
+++ MyFirstTestProject/Lesson10test.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyFirstApplication;
 
+
+// Lesson 17 - Unit Testing 
+
 namespace MyFirstTestProject
 {
     [TestClass]
@@ -24,5 +27,44 @@ namespace MyFirstTestProject
 
 
         }
+
     }
+    /*
+     Problem 1 -  Write a test method inside the Lesson10Test.cs file. Ensure you
+     have the [Test method] above this method. This method will be called
+     iDVerify. We will be utilizing the arrange and assert portions of the AAA pattern.
+     Create a variable of int called id. Provide a value for this variable.
+     Create an object of Lesson 10 using the default constructor. Using the object
+     created, assign the id variable to the ID property. Using an Assert, we are 
+     going to check to see if the ID property was assigned properly, or if its 
+     still using the default value of 1000. Pick through the Assert methods to 
+     determine which would fit. There are more than 1 possible methods to test this,
+     including the one used in the lecture. Run the test several times to ensure
+     you get both a passing test and a failure. Ensure you provide a passing version
+     in your checked code. You only need to provide 1 assert + method.
+     */
+    [TestClass]
+    public class Lesson10
+    {
+        [TestMethod]
+        public void IDVerify()
+        {
+            // arragne
+            int id = 234;
+            Lesson10 lesson10 = new Lesson10(id);
+
+            // act - Nothing for this one
+
+            // assert
+            Assert.AreEqual(id, lesson10.ID);
+
+
+        }
+
+    }
+
+
+    
+
+
 }
